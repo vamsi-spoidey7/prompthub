e76afd7e005166adca932a0e92795e8ac0063d6c
JaiLakshmanKumar <lakshmankumarkurla@gmail.com>
2024-03-16 19:08:32 +0530

    implemented hero section

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -34,7 +34,7 @@ return (
     {navItems.map((item, index) => (
         <Link key={item.title} href={item.href}>
         <h5
-            className={`inline-block md:px-4 xl:px-8 py-5 md:py-0 text-[18px] font-[500] font-Inter ${
+            className={`flex flex-col md:px-10 xl:px-12 py-5 md:py-0 text-[18px] font-[500] font-Inter ${
             activeItem === index && "text-[#6dff4b]"
             }`}
         >
